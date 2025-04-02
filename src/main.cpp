3fdc920a32593ae2f2d4a1a6475aee66879cc298
Daniil <Daniilsemen@gmail.com>
2025-04-02 19:29:45 +0300

    polish: `main.cpp`

--- src/main.cpp
+++ src/main.cpp
@@ -51,13 +51,13 @@
 namespace asio  = boost::asio;
 namespace beast = boost::beast;
 
-#define   PORT "80"
 #define   HOST "api.openbudget.gov.ua"
-#define TARGET "/api/public/localBudgetData"
+#define TARGET "/api/public/"
+#define    API "localBudgetData"
 #define     P0 "?budgetCode=2555900000"
 #define     P1 "&budgetItem=INCOMES"
 #define     P2 "&period=QUARTER"
-#define     P3 "&year=2024"
+#define     P3 "&year=2023"
 
 //constexpr auto get = "https://api.openbudget.gov.ua/api/public/localBudgetData?budgetCode=2555900000&budgetItem=INCOMES&period=QUARTER&year=2024";
 
@@ -79,26 +79,34 @@ int main()
 		beast::http::request<beast::http::empty_body> request;
 		request.method(beast::http::verb::get);
 		request.version(11);
-		/*
-		request.target(TARGET P0 P1 P2 P3);*/
-		request.target("/api/public/localBudgetData?budgetCode=2555900000&budgetItem=INCOMES&period=QUARTER&year=2024");
+
+		request.target(TARGET API P0 P1 P2 P3);
+		//request.target("/api/public/localBudgetDataa?budgetCode=2555900000&budgetItem=INCOMES&period=QUARTER&year=2024");
 		request.set(beast::http::field::host      , HOST);
 		request.set(beast::http::field::user_agent, BOOST_BEAST_VERSION_STRING);
 		request.keep_alive(false);
 
-		const auto     resolved = resolver.resolve(HOST, PORT);
+		const auto     resolved = resolver.resolve(HOST, "80");
 		stream.connect(resolved);
 		beast::http::write(stream, request);
 
 		/*
 		beast::http::response<beast::http::   file_body> response;*/
 		beast::http::response<beast::http::dynamic_body> response;
-		beast::flat_buffer buffer;
+		beast::flat_buffer buffer(1'000'000); // 1MB file limit
 
 		beast::http::read(stream, buffer, response);
 		stream.socket().shutdown(asio::ip::tcp::socket::shutdown_both);
 
 		std::cout << response << std::endl;
+		std::cout << beast::buffers_to_string(response.body().data()) << std::endl;
+
+		//std::cout << std::endl;
+		//std::cout << response.body().    size() << std::endl;
+		//std::cout << response.body().max_size() << std::endl;
+		//std::cout << buffer.capacity() << std::endl;
+		//std::cout << buffer.    size() << std::endl;
+		//std::cout << buffer.max_size() << std::endl;
 	}
 	catch (std::exception& e) { std::cerr << "Exception: " << e.what() << "\n"; }
 
